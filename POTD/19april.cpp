1756926ee0cef753515dfa471f4cf93e48eb746e
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-04-19 22:53:12 +0530

    add potd

---
+++ POTD/19april.cpp
@@ -0,0 +1,19 @@
+/*   2563. Count the Number of Fair Pairs
+
+class Solution {
+    public:
+        long long countFairPairs(vector<int>& nums, int lower, int upper) {
+            
+            sort(nums.begin(), nums.end());
+            long long ans =0;
+            for(int i =0; i<nums.size(); i++){
+                int lwr= lower-nums[i], upr = upper-nums[i];
+    
+                ans += upper_bound(nums.begin()+(i+1), nums.end(), upr)
+                -lower_bound(nums.begin()+(i+1), nums.end(), lwr);
+            }
+            return ans;
+        }
+    };
+
+    */
